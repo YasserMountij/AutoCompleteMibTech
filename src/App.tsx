9fe7e34060e1e3ad3d54bb7cbb4e3cc2ec2aba0b
Yasser Mountij <yasserm.dev@gmail.com>
2024-06-22 16:56:27 +0100

    Basic AutoComplete component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import AutoComplete from "./components/AutoComplete/AutoComplete";
+
 function App() {
-  return <div> Hello world </div>;
+  const options = [
+    { label: "test1", value: "TEST1", key: 1 },
+    { label: "test2", value: "TEST2", key: 2 },
+    { label: "test3", value: "TEST3", key: 3 },
+    { label: "test4", value: "TEST1", key: 4 },
+    { label: "test5", value: "TEST2", key: 5 },
+    { label: "test6", value: "TEST3", key: 6 },
+    { label: "test7", value: "TEST1", key: 7 },
+    { label: "test8", value: "TEST2", key: 8 },
+    { label: "test9", value: "TEST3", key: 9 },
+    { label: "test10", value: "TEST1", key: 10 },
+    { label: "test11", value: "TEST2", key: 11 },
+    { label: "test12", value: "TEST3", key: 12 },
+  ];
+  return (
+    <div className="min-h-svh w-full flex justify-center items-center bg-neutral-700 ">
+      <AutoComplete multiple={true} options={options} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/AutoComplete/AutoComplete.tsx
+++ src/components/AutoComplete/AutoComplete.tsx
@@ -1,5 +1,75 @@
-const AutoComplete = () => {
-  return <div>AutoComplete</div>;
-};
+import { useAutocomplete } from "@mui/base";
+import React from "react";
+import { unstable_useForkRef as useForkRef } from "@mui/utils";
+import { Button } from "@mui/base/Button";
+import { Popper } from "@mui/base/Popper";
+import {
+  IoChevronDownOutline,
+  IoChevronUpOutline,
+  IoCloseOutline,
+} from "react-icons/io5";
+
+const AutoComplete = React.forwardRef(function AutoComplete(
+  props: any,
+  ref: React.ForwardedRef<HTMLDivElement>
+) {
+  /** hooks */
+  const {
+    expanded,
+    focusedTag,
+    getInputLabelProps,
+    getTagProps,
+    inputValue,
+    value,
+    getRootProps,
+    getInputProps,
+    getPopupIndicatorProps,
+    getClearProps,
+    getListboxProps,
+    getOptionProps,
+    dirty,
+    id,
+    popupOpen,
+    focused,
+    anchorEl,
+    setAnchorEl,
+    groupedOptions,
+  } = useAutocomplete({
+    ...props,
+  });
+
+  const rootRef = useForkRef(ref, setAnchorEl);
+  return (
+    <>
+      <div ref={rootRef}>
+        <input
+          id={id}
+          // disabled={disabled}
+          // readOnly={readOnly}
+          {...getInputProps()}
+        />
+        <Button {...getClearProps()}>
+          <IoCloseOutline />
+        </Button>
+        <Button {...getPopupIndicatorProps()}>
+          {popupOpen ? <IoChevronUpOutline /> : <IoChevronDownOutline />}
+        </Button>
+      </div>
+      {anchorEl && (
+        <Popper open={popupOpen} anchorEl={anchorEl}>
+          <ul {...getListboxProps()}>
+            {groupedOptions.map((option, index) => {
+              const optionProps = getOptionProps({ option, index });
+
+              return <li {...optionProps}>{option.label}</li>;
+            })}
+
+            {groupedOptions.length === 0 && <li>No results</li>}
+          </ul>
+        </Popper>
+      )}
+    </>
+  );
+});
 
 export default AutoComplete;
