492ef28483b5ad32e162eab7326126bc5e99cd2d
Yasser Mountij <yasserm.dev@gmail.com>
2024-06-24 05:14:04 +0100

    rename AutoComplete.tsx > index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import AutoComplete from "./components/AutoComplete/AutoComplete";
+import AutoComplete from "./components/AutoComplete";
 import {
   IoFlameOutline,
   IoHelp,
